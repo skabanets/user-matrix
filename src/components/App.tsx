613392b1d3f828c58a3ce1f948fde780d0ef577d
Sergey Kabanets <skabanets97@gmail.com>
2024-09-04 21:49:56 +0300

    Add fonts

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,7 @@
 export const App = () => {
   return (
-    <div className="h-screen flex justify-center items-center">
-      <h1 className="text-4xl">User-Matrix</h1>
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="font-roboto-regular text-4xl">User-Matrix</h1>
     </div>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,10 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      "roboto-regular": ["Roboto-Regular", "sans-serif"],
+      "roboto-bold": ["Roboto-Bold", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
