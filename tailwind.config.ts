2039e1aa695a72a3fe52b7e65830232ec6e9e1da
Sergey Kabanets <skabanets97@gmail.com>
2024-09-04 22:16:54 +0300

    Add variables for colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,47 @@ export default {
       "roboto-regular": ["Roboto-Regular", "sans-serif"],
       "roboto-bold": ["Roboto-Bold", "sans-serif"],
     },
-    extend: {},
+    screens: {
+      md: "768px",
+      lg: "1440px",
+    },
+    extend: {
+      colors: {
+        primaryBgColor: "var(--background-color)",
+        primaryTextColor: "var(--primary-text-color)",
+        tableHeaderBgColor: "var(--table-header-background-color)",
+        tableHeaderTextColor: "var(--table-header-text-color)",
+        tableRowOddColor: "var(--table-row-odd-color)",
+        tableRowEvenColor: "var(--table-row-even-color)",
+        greyColor: "var(--grey-color)",
+      },
+    },
   },
-  plugins: [],
+  corePlugins: {
+    container: false,
+  },
+  plugins: [
+    ({ addComponents }) => {
+      addComponents({
+        ".container": {
+          minWidth: "320px",
+          maxWidth: "375px",
+          marginLeft: "auto",
+          marginRight: "auto",
+          paddingLeft: "20px",
+          paddingRight: "20px",
+          "@screen md": {
+            paddingLeft: "32px",
+            paddingRight: "32px",
+            maxWidth: "768px",
+          },
+          "@screen lg": {
+            paddingLeft: "100px",
+            paddingRight: "100px",
+            maxWidth: "1440px",
+          },
+        },
+      });
+    },
+  ],
 };
