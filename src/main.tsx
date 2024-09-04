5f68c3d0c893b009316aa09e2c257cb945c264fd
Sergey Kabanets <skabanets97@gmail.com>
2024-09-04 23:32:00 +0300

    Add redux store and hooks for redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 
 import { App } from "./components/App";
 
+import { store } from "./redux/store";
 import "./index.css";
 import "modern-normalize/modern-normalize.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <>
+  <Provider store={store}>
     <App />
-  </>
+  </Provider>
 );
