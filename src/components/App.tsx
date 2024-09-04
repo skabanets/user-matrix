0e3ce0b88b69534f980fc205c1517cacead32944
Sergey Kabanets <skabanets97@gmail.com>
2024-09-04 23:34:26 +0300

    Add user data fetching on initial render

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,22 +1,18 @@
+import { useEffect } from "react";
+
 import { Table } from "./Table/Table";
 
+import { fetchUsers } from "../redux/users/usersOperations";
+import { selectUsers } from "../redux/users/usersSlice";
+import { useAppDispatch, useAppSelector } from "../hooks/reduxHooks";
+
 export const App = () => {
-  const users = [
-    {
-      id: 1,
-      name: "Leanne Graham",
-      username: "Bret",
-      email: "Sincere@april.biz",
-      phone: "1-770-736-8031 x56442",
-    },
-    {
-      id: 2,
-      name: "Ervin Howell",
-      username: "Antonette",
-      email: "Shanna@melissa.tv",
-      phone: "010-692-6593 x09125",
-    },
-  ];
+  const users = useAppSelector(selectUsers);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
 
   return (
     <main>
