0608476ab60e3000cbf2343f05a8845e01495eca
Sergey Kabanets <skabanets97@gmail.com>
2024-09-05 19:01:15 +0300

    Add Filetrs value to usersSlice

--- src/redux/users/usersSlice.ts
+++ src/redux/users/usersSlice.ts
@@ -3,20 +3,38 @@ import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { fetchUsers } from "./usersOperations";
 import type { User } from "../../types/User";
 
+export interface Filters {
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+}
+
 interface UserSlice {
   users: User[];
+  filters: Filters;
   isLoading: boolean;
 }
 
 const initialState: UserSlice = {
   users: [],
+  filters: {
+    name: "",
+    username: "",
+    email: "",
+    phone: "",
+  },
   isLoading: false,
 };
 
-const userSLice = createSlice({
+const userSlice = createSlice({
   name: "users",
   initialState,
-  reducers: {},
+  reducers: {
+    updateFilters: (state, action: PayloadAction<{ field: keyof Filters; value: string }>) => {
+      state.filters[action.payload.field] = action.payload.value;
+    },
+  },
   extraReducers: builder => {
     builder
       .addCase(fetchUsers.pending, state => {
@@ -33,8 +51,10 @@ const userSLice = createSlice({
   selectors: {
     selectUsers: (state: UserSlice) => state.users,
     selectIsLoading: (state: UserSlice) => state.isLoading,
+    selectFilters: (state: UserSlice) => state.filters,
   },
 });
 
-export const usersReducer = userSLice.reducer;
-export const { selectUsers, selectIsLoading } = userSLice.selectors;
+export const usersReducer = userSlice.reducer;
+export const { updateFilters } = userSlice.actions;
+export const { selectUsers, selectIsLoading, selectFilters } = userSlice.selectors;
